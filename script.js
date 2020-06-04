b6c0ef6fd84b8843e427386558eb6857e653fd77
taylorhl <hbrecht2@live.maryville.edu>
2020-06-03 21:37:09 -0400

    Updated home page styling

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 window.onload = function(){
     var navIcon = document.getElementById("navIcon")
-    navIcon.addEventListener("click", showHiddenNavMenu())
+    navIcon.addEventListener("click", showHiddenNavMenu)
 }
 
+//Displays nav menu when user clicks on menu icon
 function showHiddenNavMenu() {
     var hiddenNavMenu = document.getElementById("hiddenNavMenu");
-    hiddenNavMenu.classList.remove("hidden")
+    if (hiddenNavMenu.classList.contains("hidden")){
+        hiddenNavMenu.classList.remove("hidden")
+    } else {
+        hiddenNavMenu.classList.add("hidden")
+    }
 }
-
