b39e29067f74440b1466cdf5dd780c41aaabdc05
Hannah Brecht <hbrecht2@live.maryville.edu>
2020-10-02 15:01:34 -0400

    Added ClearDB Addon and Exported SQL file for Trivia Geek DB from localhost to clearDB

--- connect.php
+++ connect.php
@@ -1,12 +1,16 @@
 <?php
-// PHP code to connect to my tri\ia database
-$serverName = "localhost";
-$username = "root";
-$password = "root";
-$dbName = "triviageek";
-$db = new PDO("mysql:host=localhost;dbname=triviageek", $username, $password);
+// PHP code to connect to clearDB triviageek db
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
 
+$db = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
 
 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
+
+
+
